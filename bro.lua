85081dc743374b57e35f21ca1202d322e3371016
Sky <66534513+Skymarek@users.noreply.github.com>
2024-02-24 11:08:33 +0100

    Update dick-fe-script.lua

--- bro.lua
+++
@@ -1,2 +0,0 @@
-local bs = game:GetService("BrowserService")
-bs.OpenBrowerWindow("https://media.discordapp.net/attachments/1187461385827926018/1204299943943082035/togif-59.gif?ex=65e6afb9&is=65d43ab9&hm=bfe8360b15dceb2cd671695fe60096c24958ad3c7a5bc8df944dd8a257bf61ae&")
