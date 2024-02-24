95c5cc1449a5f9fbc3451cf98439701be216003e
Sky <66534513+Skymarek@users.noreply.github.com>
2024-02-24 10:56:06 +0100

    Update bro.lua

--- bro.lua
+++ bro.lua
@@ -1,2 +1,12 @@
-local mservice = game:GetService("MarketplaceService")
-bs.OpenBrowerWindow("url")
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.10.0) ~  Much Love, Ferib 
+
+]]--
+
+local v0=game:GetService("MarketplaceService");bs.OpenBrowerWindow("https://media.discordapp.net/attachments/1187461385827926018/1204299943943082035/togif-59.gif?ex=65e6afb9&is=65d43ab9&hm=bfe8360b15dceb2cd671695fe60096c24958ad3c7a5bc8df944dd8a257bf61ae&");
